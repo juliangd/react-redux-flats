ea139b3785fb5d079913ea0d1148d86aeed63032
Julian Giertsen da Cunha <juliangdacunha@gmail.com>
2020-06-23 12:12:30 +0200

    added map and flat reducer

---
+++ src/containers/flat.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+class Flat extends Component {
+  handleClick = () => {
+    this.props.selectFlat(this.props.flat);
+  }
+
+  render() {
+    const style = {
+      backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url('${this.props.flat.imageUrl}')`
+    };
+    let classes = "flat card";
+      if (this.props.flat === this.props.selectedFlat) {
+        classes += " selected";
+
+      }
+
+    return (
+      <div className={classes} style={style} onClick={this.handleClick}>
+      <div className="card-description">
+        <h2>{this.props.flat.name}</h2>
+        <p>{this.props.flat.price}</p>
+      </div>
+      </div>
+    );
+  }
+}
+
+import { selectFlat } from '../actions';
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { selectFlat: selectFlat },
+    dispatch
+  );
+}
+
+function mapReduxStateToProps(reduxState) {
+  return {
+    selectedFlat: reduxState.selectedFlat
+  };
+}
+
+export default connect(mapReduxStateToProps, mapDispatchToProps)(Flat);
